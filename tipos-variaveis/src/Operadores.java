9533641d17af5fe0e703a4774ab496a047d89f2a
joilson <jfiel.ti@gmail.com>
2024-05-14 15:15:42 -0300

    atualizacao

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+      int a, b;
+      a = 8;
+      b = 6;
+      int resultado = a==b ? 1 : 2;
+      System.out.println(resultado);
+    }
+
+}
